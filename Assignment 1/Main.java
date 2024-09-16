4914cb8da55b2d53bb88cc00949d015608e86e4f
ayush-1801 <ayush23162@iiitd.ac.in>
2024-09-17 00:09:48 +0530

    Created data initialization function

--- Assignment 1/Main.java
+++ Assignment 1/Main.java
@@ -1,48 +1,6 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Props.courses = new ArrayList<ArrayList<Course>>();
-        for (int i = 0; i < 8; i++) {
-            Props.courses.add(new ArrayList<Course>());
-        }
-        Props.students = new ArrayList<Student>();
-        Props.professors = new ArrayList<Professor>();
-        Props.admins = new ArrayList<Admin>();
-
-        Admin admin = new Admin("admin@iiitd.ac.in", "methhead");
-        Props.addAdmin(admin);
-
-        Professor prof1 = new Professor("Sambuddho", "Chakravarty", "sambuddho@iiitd.ac.in", "sambuddho123");
-        Professor prof2 = new Professor("Raghava", "Mutharaju", "raghava@iiitd.ac.in", "raghava456");
-        Props.addProfessor(prof1);
-        Props.addProfessor(prof2);
-
-        Student stud1 = new Student("Arhan", "Jain", "arhan@iiitd.ac.in", "arhan123");
-        Student stud2 = new Student("Arnesh", "Batra", "arnesh@iiitd.ac.in", "arnesh123");
-        Student stud3 = new Student("Dev", "Sharma", "dev@iiitd.ac.in", "dev123");
-        Props.addStudent(stud1);
-        Props.addStudent(stud2);
-        Props.addStudent(stud3);
-
-        Props.courses.get(1).add(new Course("CSE101", "Introduction to Programming", 4, "Pankaj Jalote", 1));
-        Props.courses.get(1).add(new Course("MTH100", "Linear Algebra", 4, "Subhajit Ghoosechowdhary", 1));
-        Props.courses.get(1).add(new Course("DES102", "Introduction to Human-Computer Interaction", 4, "Rajiv Ratn Shah", 1));
-        Props.courses.get(1).add(new Course("COM101", "Communication Skills", 4, "Payal Mukherjee", 1));
-        Props.courses.get(1).add(new Course("ECE111", "Digital Circuits", 4, "Pravesh Biyani", 1));
-
-        Props.courses.get(2).add(new Course("CSE102", "Data Structures and Algorithms", 4, "Ojaswa Sharma", 2));
-        Props.courses.get(2).add(new Course("MTH201", "Probability and Statistics", 4, "Subhajit Ghoosechowdhary", 2));
-        Props.courses.get(2).add(new Course("CSE112", "Computer Organization", 4, "Sujay Deb", 2));
-        Props.courses.get(2).add(new Course("ECO223", "Money and Banking", 4, "Kirti Kanjilal", 2));
-        Props.courses.get(2).add(new Course("CSE140", "Introduction to Intelligent Systems", 4, "Jainender Shukla", 2));
-
-        Props.courses.get(3).add(new Course("CSE231", "Operating Systems", 4, "Raghava Mutharaju", 3));
-        Props.courses.get(3).add(new Course("CSE201", "Advanced Programming", 4, "Sambuddho Chakravarty", 3));
-        Props.courses.get(3).add(new Course("MTH210", "Discrete Structures", 4, "Ashish Kumar Pandey", 3));
-        Props.courses.get(3).add(new Course("MTH203", "Multivariable Calculus", 4, "Sarthok Sirkar", 3));
-        Props.courses.get(3).add(new Course("ECE250", "Signals and Systems", 4, "Anubha Gupta", 3));
-
+        Props.initializeData();
         Props.introScreen();
         Props.login();
     }
--- Assignment 1/Props.java
+++ Assignment 1/Props.java
@@ -20,7 +20,7 @@ public class Props {
 
     public static ArrayList<Professor> getProfessors() {
         return professors;
-    }   
+    }
 
     public static ArrayList<Admin> getAdmins() {
         return admins;
@@ -73,13 +73,16 @@ public class Props {
 
     public static void printHeader(String title) {
         clear();
-        System.out.println("============================================================================================================");
+        System.out.println(
+                "============================================================================================================");
         System.out.println("                                                " + title);
-        System.out.println("============================================================================================================");
+        System.out.println(
+                "============================================================================================================");
     }
 
     public static void printFooter() {
-        System.out.println("============================================================================================================");
+        System.out.println(
+                "============================================================================================================");
         System.out.print("Press Enter to continue...");
         scanner.nextLine();
     }
@@ -97,12 +100,18 @@ public class Props {
     // Screen methods
     public static void introScreen() {
         clear();
-        System.out.println("   ___                              __            _     _             _   _               __           _                 ");
-        System.out.println("  / __\\___  _   _ _ __ ___  ___    /__\\ ___  __ _(_)___| |_ _ __ __ _| |_(_) ___  _ __   / _\\_   _ ___| |_ ___ _ __ ___  ");
-        System.out.println(" / /  / _ \\| | | | '__/ __|/ _ \\  / \\/// _ \\/ _` | / __| __| '__/ _` | __| |/ _ \\  '_ \\  \\ \\| | | / __| __/ _ \\ '_ ` _ \\ ");
-        System.out.println("/ /__| (_) | |_| | |  \\__ \\  __/ / _  \\  __/ (_| | \\__ \\ |_| | | (_| | |_| | (_) | | | | _\\ \\ |_| \\__ \\ ||  __/ | | | | |");
-        System.out.println("\\____/\\___/ \\__,_|_|  |___/\\___| \\/ \\_/\\___|\\___,|_|___/\\__|_|  \\__,_|\\__|_|\\___/|_| |_| \\__/\\__, |___/\\__\\___|_| |_| |_|");
-        System.out.println("                                            |___/                                            |___/                       ");
+        System.out.println(
+                "   ___                              __            _     _             _   _               __           _                 ");
+        System.out.println(
+                "  / __\\___  _   _ _ __ ___  ___    /__\\ ___  __ _(_)___| |_ _ __ __ _| |_(_) ___  _ __   / _\\_   _ ___| |_ ___ _ __ ___  ");
+        System.out.println(
+                " / /  / _ \\| | | | '__/ __|/ _ \\  / \\/// _ \\/ _` | / __| __| '__/ _` | __| |/ _ \\  '_ \\  \\ \\| | | / __| __/ _ \\ '_ ` _ \\ ");
+        System.out.println(
+                "/ /__| (_) | |_| | |  \\__ \\  __/ / _  \\  __/ (_| | \\__ \\ |_| | | (_| | |_| | (_) | | | | _\\ \\ |_| \\__ \\ ||  __/ | | | | |");
+        System.out.println(
+                "\\____/\\___/ \\__,_|_|  |___/\\___| \\/ \\_/\\___|\\___,|_|___/\\__|_|  \\__,_|\\__|_|\\___/|_| |_| \\__/\\__, |___/\\__\\___|_| |_| |_|");
+        System.out.println(
+                "                                            |___/                                            |___/                       ");
         System.out.print("Press Enter to continue...");
         scanner.nextLine();
     }
@@ -126,14 +135,19 @@ public class Props {
             clear();
 
             if (choice == 1) {
-                printHeader("Login");   
+                printHeader("Login");
                 System.out.println("Select user type:");
                 System.out.println("1. Student");
                 System.out.println("2. Professor");
                 System.out.println("3. Admin");
-                System.out.print("Enter your choice (1-3): ");
+                System.out.println("4. Back");
+                System.out.print("Enter your choice (1-5): ");
                 int user = scanner.nextInt();
                 scanner.nextLine();
+
+                if (user == 4) {
+                    continue;
+                }
 
                 System.out.print("Enter username: ");
                 String username = scanner.nextLine();
@@ -156,6 +170,8 @@ public class Props {
             } else if (choice == 2) {
                 signup();
             } else if (choice == 3) {
+                continue;
+            } else if (choice == 4) {
                 exitScreen();
             } else {
                 System.out.println("Invalid choice. Please try again.");
@@ -169,9 +185,19 @@ public class Props {
         System.out.println("Select user type:");
         System.out.println("1. Student");
         System.out.println("2. Professor");
-        System.out.print("Enter your choice (1-2): ");
-        int userType = scanner.nextInt();
+        System.out.println("3. Back");
+        System.out.print("Enter your choice (1-3): ");
+        int user = scanner.nextInt();
         scanner.nextLine();
+
+        if (user == 3) {
+            return;
+        }
+
+        if (user != 1 && user != 2) {
+            System.out.println("Invalid user type. Signup failed.");
+            return;
+        }
 
         System.out.print("Enter first name: ");
         String firstName = scanner.nextLine();
@@ -182,14 +208,14 @@ public class Props {
         System.out.print("Enter password: ");
         String password = scanner.nextLine();
 
-        if (userType == 1) {
+        if (user == 1) {
             students.add(new Student(firstName, lastName, email, password));
-            System.out.println("Congratulations! Your student account has been created successfully. You may now proceed to log in.");
-        } else if (userType == 2) {
-            professors.add(new Professor(firstName, lastName, email, password));
-            System.out.println("Congratulations! Your professor account has been created successfully. You may now proceed to log in.");
+            System.out.println(
+                    "Congratulations! Your student account has been created successfully. You may now proceed to log in.");
         } else {
-            System.out.println("Invalid user type. Signup failed.");
+            professors.add(new Professor(firstName, lastName, email, password));
+            System.out.println(
+                    "Congratulations! Your professor account has been created successfully. You may now proceed to log in.");
         }
     }
 
@@ -214,10 +240,10 @@ public class Props {
             System.out.println("6. Submit a complaint");
             System.out.println("7. Logout");
             System.out.print("Enter your choice (1-7): ");
-            
+
             int choice = scanner.nextInt();
             scanner.nextLine();
-            
+
             switch (choice) {
                 case 1:
                     student.viewAvailableCourses();
@@ -262,10 +288,10 @@ public class Props {
             System.out.println("2. View enrolled students");
             System.out.println("3. Logout");
             System.out.print("Enter your choice (1-3): ");
-            
+
             int choice = scanner.nextInt();
             scanner.nextLine();
-            
+
             switch (choice) {
                 case 1:
                     professor.manageCourses();
@@ -302,10 +328,10 @@ public class Props {
             System.out.println("6. Handle complaints");
             System.out.println("7. Logout");
             System.out.print("Enter your choice (1-7): ");
-            
+
             int choice = scanner.nextInt();
             scanner.nextLine();
-            
+
             switch (choice) {
                 case 1:
                     admin.addCourse();
@@ -331,5 +357,48 @@ public class Props {
                     System.out.println("Invalid choice. Please try again.");
             }
         }
+    }
+
+    public static void initializeData() {
+        Props.courses = new ArrayList<ArrayList<Course>>();
+        for (int i = 0; i < 8; i++) {
+            Props.courses.add(new ArrayList<Course>());
+        }
+        Props.students = new ArrayList<Student>();
+        Props.professors = new ArrayList<Professor>();
+        Props.admins = new ArrayList<Admin>();
+
+        Admin admin = new Admin("admin@iiitd.ac.in", "methhead");
+        Props.addAdmin(admin);
+
+        Professor prof1 = new Professor("Sambuddho", "Chakravarty", "sambuddho@iiitd.ac.in", "sambuddho123");
+        Professor prof2 = new Professor("Raghava", "Mutharaju", "raghava@iiitd.ac.in", "raghava456");
+        Props.addProfessor(prof1);
+        Props.addProfessor(prof2);
+
+        Student stud1 = new Student("Arhan", "Jain", "arhan@iiitd.ac.in", "arhan123");
+        Student stud2 = new Student("Arnesh", "Batra", "arnesh@iiitd.ac.in", "arnesh123");
+        Student stud3 = new Student("Dev", "Sharma", "dev@iiitd.ac.in", "dev123");
+        Props.addStudent(stud1);
+        Props.addStudent(stud2);
+        Props.addStudent(stud3);
+
+        Props.courses.get(1).add(new Course("CSE101", "Introduction to Programming", 4, "Pankaj Jalote", 1));
+        Props.courses.get(1).add(new Course("MTH100", "Linear Algebra", 4, "Subhajit Ghoosechowdhary", 1));
+        Props.courses.get(1).add(new Course("DES102", "Introduction to Human-Computer Interaction", 4, "Rajiv Ratn Shah", 1));
+        Props.courses.get(1).add(new Course("COM101", "Communication Skills", 4, "Payal Mukherjee", 1));
+        Props.courses.get(1).add(new Course("ECE111", "Digital Circuits", 4, "Pravesh Biyani", 1));
+
+        Props.courses.get(2).add(new Course("CSE102", "Data Structures and Algorithms", 4, "Ojaswa Sharma", 2));
+        Props.courses.get(2).add(new Course("MTH201", "Probability and Statistics", 4, "Subhajit Ghoosechowdhary", 2));
+        Props.courses.get(2).add(new Course("CSE112", "Computer Organization", 4, "Sujay Deb", 2));
+        Props.courses.get(2).add(new Course("ECO223", "Money and Banking", 4, "Kirti Kanjilal", 2));
+        Props.courses.get(2).add(new Course("CSE140", "Introduction to Intelligent Systems", 4, "Jainender Shukla", 2));
+
+        Props.courses.get(3).add(new Course("CSE231", "Operating Systems", 4, "Raghava Mutharaju", 3));
+        Props.courses.get(3).add(new Course("CSE201", "Advanced Programming", 4, "Sambuddho Chakravarty", 3));
+        Props.courses.get(3).add(new Course("MTH210", "Discrete Structures", 4, "Ashish Kumar Pandey", 3));
+        Props.courses.get(3).add(new Course("MTH203", "Multivariable Calculus", 4, "Sarthok Sirkar", 3));
+        Props.courses.get(3).add(new Course("ECE250", "Signals and Systems", 4, "Anubha Gupta", 3));
     }
 }
